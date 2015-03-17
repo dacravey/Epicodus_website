f96c42115f278d080c72425d083fd36130982f89
Debbie Cravey and Luke Chinworth <lukechinworth@gmail.com>
2015-03-17 11:35:44 -0700

    add ability to add lesson via form

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,4 +6,24 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
   end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      flash[:notice] = "Lesson successfully added!"
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :body)
+  end
 end
